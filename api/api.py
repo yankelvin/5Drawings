4bdee98d6c7549b3c9b6390708098450f2b5c20e
Yan Kelvin <yan.kelvin@hotmail.com>
2021-06-02 16:54:27 -0300

    Removendo cors

--- api/api.py
+++ api/api.py
@@ -1,17 +1,14 @@
 import time
 from flask import Flask, request
-from flask_cors import CORS, cross_origin
 
 from model.cnn import Cnn
 
 app = Flask(__name__)
-cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 cnn = Cnn()
 
 
 @app.route('/api/predict', methods=['POST'])
-@cross_origin()
 def predict():
     raw_data = request.files['file']
 
